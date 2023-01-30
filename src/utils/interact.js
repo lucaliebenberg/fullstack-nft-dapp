6de2c06a0632dd511cb1d7be4c854ef047e7eb2b
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-30 16:09:48 +0200

    mintNFT add to /interact.js file

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -1,3 +1,13 @@
+import { pinJSONToIPFS } from "./pinata.js";
+
+require("dotenv").config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE";
+
 export const connectWallet = async () => {
   if (window.ethereum) {
     try {
@@ -72,6 +82,62 @@ export const getCurrentWalletConnected = async () => {
           </p>
         </span>
       ),
+    };
+  }
+};
+
+export const mintNFT = async (url, name, description) => {
+  //error handling
+  if (url.trim() === "" || name.trim() == "" || description.trim() == "") {
+    return {
+      success: false,
+      status: "❗Please make sure all fields are completed before minting.",
+    };
+  }
+
+  //make metadata
+  const metadata = new Object();
+  metadata.name = name;
+  metadata.image = url;
+  metadata.description = description;
+
+  //make pinata call
+  const pinataResponse = await pinJSONToIPFS(metadata);
+  if (!pinataResponse.success) {
+    return {
+      success: false,
+      status: "😢 Something went wrong while uploading your tokenURI.",
+    };
+  }
+  const tokenURI = pinataResponse.pinataUrl;
+
+  window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+  //set up your Ethereum transaction
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    data: window.contract.methods
+      .mintNFT(window.ethereum.selectedAddress, tokenURI)
+      .encodeABI(), //make call to NFT smart contract
+  };
+
+  //sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status:
+        "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+        txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
     };
   }
 };
